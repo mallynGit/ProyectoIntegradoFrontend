42178928d7be7003b7a5b942821c57f8e58b8262
mallynGit <miguelitorruiz@gmail.com>
2024-04-28 23:46:26 +0200

    login fix

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,40 +1,70 @@
 <template>
-    <div class="relative-position" style="border: 1px solid cyan; height:100%;">
-
-
-        <div style="border: 2px solid blueviolet; min-height: 75%; min-width: 25%;" class="absolute-center q-pa-sm">
-            <q-form>
-                <q-input class="input" v-model="loginForm.login" filled label="Nombre o email" />
-                <q-separator />
-                <q-input class="input" v-model="loginForm.password" filled label="Contrase;a" />
-                <q-checkbox class="input" label="Recordarme" v-model="recordar" />
-                <q-separator />
-                <div class="button-container q-pa-md">
-                    <q-btn class="input" label="Login" />
-                    <q-btn class="input"  label="Register" to="/auth/register" />
-                </div>
-            </q-form>
+  <div class="relative-position" style="border: 1px solid cyan; height: 100%">
+    <div
+      style="border: 2px solid blueviolet; min-height: 75%; min-width: 25%"
+      class="absolute-center q-pa-sm"
+    >
+      <q-form @submit="login">
+        <q-input
+          class="input"
+          v-model="loginForm.email"
+          filled
+          label="Nombre o email"
+        />
+        <q-separator />
+        <q-input
+          class="input"
+          v-model="loginForm.password"
+          filled
+          label="Contrase;a"
+        />
+        <q-checkbox class="input" label="Recordarme" v-model="recordar" />
+        <q-separator />
+        <div class="button-container q-pa-md">
+          <q-btn class="input" label="Login" type="submit" />
+          <q-btn class="input" label="Register" to="/auth/register" />
         </div>
-
+      </q-form>
     </div>
+  </div>
 </template>
 
 <script setup>
-let recordar = ref(false)
+const { $axios } = useNuxtApp();
+const router = useRouter();
+let recordar = ref(false);
 
 let loginForm = ref({
-    login: "",
-    password: "",
-})
+  email: "",
+  password: "",
+});
+
+const login = async () => {
+  try {
+    let login = loginForm.value.login;
+
+    let res = await $axios.post("/auth/login", loginForm.value);
+    console.log(res);
+
+    alert("Login correcto");
+  } catch (err) {
+    console.log(err, "err");
+    if (err.response.status == 404) {
+      alert("Usuario no encontrado");
+    } else if (err.response.status == 401) {
+      alert("Hubo un error con el servidor");
+    }
+  }
+};
 </script>
 
 <style scoped>
-.button-container{
-    display: flex;
-    align-items: center;
-    justify-content: space-evenly;
+.button-container {
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
 }
 .input {
-    margin: 5px 0px;
+  margin: 5px 0px;
 }
 </style>
--- pages/test/hola.vue
+++ pages/test/hola.vue
@@ -1,18 +1,37 @@
 <template>
-
-    <div>
-        <q-select class="q-pa-md" v-model="sel" :options="options" label="select" style="width:300px;"></q-select>
-        <div class="q-pa-md" style="border: 1px solid turquoise; display: inline-block;  height: 90%">gdf</div>
+  <div>
+    <q-select
+      class="q-pa-md"
+      v-model="sel"
+      :options="options"
+      label="select"
+      style="width: 300px"
+    ></q-select>
+    <div
+      class="q-pa-md"
+      style="border: 1px solid turquoise; display: inline-block; height: 90%"
+    >
+      gdf
     </div>
 
-
-
-
+    <q-card v-for="user of users">
+      <div>
+        <q-card-section v-for="(k, v) of user"
+          >{{ v }}: {{ k }},
+        </q-card-section>
+      </div>
+    </q-card>
+  </div>
 </template>
 
 <script setup>
+const { $axios } = useNuxtApp();
+let users = ref([]);
 
-let sel = ref("hola")
-let options = ["hola", "mundo"]
+$axios.get("/user/find").then((res) => {
+  users.value = res.data;
+});
 
+let sel = ref("hola");
+let options = ["hola", "mundo"];
 </script>
