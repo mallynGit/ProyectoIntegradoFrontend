d948cbf59f1deb81d3c6955f539ee6cdaa9a9448
mallynGit <miguelitorruiz@gmail.com>
2024-06-06 00:44:34 +0200

    socket.io funcionando

--- components/footer-chat.vue
+++ components/footer-chat.vue
@@ -1,187 +1,258 @@
 <template>
   <div class="footer row justify-end items-end">
-    <listado-usuarios v-if="openUserList" v-model="openUserList" @update:model-value="(e) => r(e)"
-      @opened="(e) => r(e)" />
+    <listado-usuarios
+      v-if="openUserList"
+      v-model="openUserList"
+      @update:model-value="(e) => r(e)"
+      @opened="(e) => r(e)"
+    />
     <div class="q-pa-sm col-6 chat" v-if="openChat != null">
-      <q-expansion-item class="conversation" :label="'Chat ' + openChat" default-opened>
+      <q-expansion-item
+        class="conversation"
+        :label="'Chat ' + openChat"
+        default-opened
+      >
         <template v-slot:header>
           <div class="chat-header">
-            {{ openChat.participantes.filter(i => i._id != useUser().getUser()._id)[0].nick }}
+            {{
+              openChat.participantes.filter(
+                (i) => i._id != useUser().getUser()._id
+              )[0].nick
+            }}
           </div>
         </template>
 
         <template v-slot:default>
           <div class="chat-content">
-            <q-virtual-scroll :items="openChat.mensajes" class="scr" v-slot="{ item, index }" ref="scrollChat">
-
+            <q-virtual-scroll
+              :items="openChat.mensajes"
+              class="scr"
+              v-slot="{ item, index }"
+              ref="scrollChat"
+            >
               <q-item
-                :class="`row no-wrap items-center justify${transformUIDToNick(item.autor, openChat) == useUser().getUser().nick ? '-start' : '-end'}`">
-                <q-chat-message :name="transformUIDToNick(item.autor, openChat)" :text="[item.contenido]"
-                  :sent="transformUIDToNick(item.autor, openChat) != useUser().getUser().nick">
+                :class="`row no-wrap items-center justify${
+                  transformUIDToNick(item.autor, openChat) ==
+                  useUser().getUser().nick
+                    ? '-start'
+                    : '-end'
+                }`"
+              >
+                <q-chat-message
+                  :name="transformUIDToNick(item.autor, openChat)"
+                  :text="[item.contenido]"
+                  :sent="
+                    transformUIDToNick(item.autor, openChat) !=
+                    useUser().getUser().nick
+                  "
+                >
                   <template v-slot:avatar>
-                    <q-img :src="`${apiUrl}/uploads/${item.autor}`" ratio="1" height="35px" width="35px"
-                      :class="`q-m${transformUIDToNick(item.autor, openChat) == useUser().getUser().nick ? 'r' : 'l'}-md`
-                        "></q-img>
+                    <q-img
+                      :src="`${apiUrl}/uploads/${item.autor}`"
+                      ratio="1"
+                      height="35px"
+                      width="35px"
+                      :class="`q-m${
+                        transformUIDToNick(item.autor, openChat) ==
+                        useUser().getUser().nick
+                          ? 'r'
+                          : 'l'
+                      }-md`"
+                    ></q-img>
                   </template>
                 </q-chat-message>
               </q-item>
-
-
-
-
             </q-virtual-scroll>
             <q-separator />
             <div class="chat-input self-center">
               <form @submit.prevent="testeo">
-                <q-input type="text" placeholder="Escribe mensaje" dense v-model="newMessage"></q-input>
+                <q-input
+                  type="text"
+                  placeholder="Escribe mensaje"
+                  dense
+                  v-model="newMessage"
+                ></q-input>
               </form>
             </div>
-
           </div>
         </template>
-
       </q-expansion-item>
     </div>
 
     <div class="q-pa-sm col-6 chats">
       <q-expansion-item class="text-h6" style="border: 1px solid salmon">
-
         <template v-slot:header>
-          <div class="justify-evenly">
-            Chats
-          </div>
+          <div class="justify-evenly">Chats</div>
         </template>
 
         <template v-slot:default>
           <div class="q-py-sm q-pl-sm q-pr-lg cvs">
             <q-list separator class="chat-list">
-              <q-item clickable v-ripple v-for="v of useUserStore().chats" :key="v._id" @click="openChat = v">
-
+              <q-item
+                clickable
+                v-ripple
+                v-for="v of useUserStore().chats"
+                :key="v._id"
+                @click="openChat = v"
+              >
                 <q-item-section avatar>
-                  <q-img ratio="1" width="50px"
-                    :src="`${apiUrl}/uploads/${v.participantes.filter(i => i._id != useUser().getUser()._id)[0]._id}`" />
+                  <q-img
+                    ratio="1"
+                    width="50px"
+                    :src="`${apiUrl}/uploads/${
+                      v.participantes.filter(
+                        (i) => i._id != useUser().getUser()._id
+                      )[0]._id
+                    }`"
+                  />
                 </q-item-section>
 
-                <q-item-section>{{
-                  v.participantes.filter(i => i._id != useUser().getUser()._id)[0].nick }}
-                  </q-item-section>
-
+                <q-item-section
+                  >{{
+                    v.participantes.filter(
+                      (i) => i._id != useUser().getUser()._id
+                    )[0].nick
+                  }}
+                </q-item-section>
               </q-item>
             </q-list>
           </div>
 
           <q-btn class="new-chat" color="green" @click="openUserList = true">
-            <q-icon name="mdi-chat-plus">
-
-            </q-icon>
+            <q-icon name="mdi-chat-plus"> </q-icon>
           </q-btn>
-
         </template>
-
-
-
       </q-expansion-item>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-const apiUrl = useRuntimeConfig().public.urlApi
+const apiUrl = useRuntimeConfig().public.urlApi;
+import { ref } from "vue";
+import io from "socket.io-client";
 
-const retrieved = await useUser().getChats()
+const socket = io(useRuntimeConfig().public.urlApi, {auth: {user: useUser().getUser()._id}});
 
+socket.on("connect", () => {
+  console.log("Conectado");
+});
 
-onMounted(() => {
-  console.log(useUserStore().chats, 'ibiien joder')
+socket.on("new msg", (data) => {
+  console.log(data, "new msg");
+  // useUserStore().chats.filter((c) => c._id == data.id)[0].mensajes.push({
+  //   autor: data.autor,
+  //   contenido: data.content,
+  // });
 })
 
+onBeforeMount(async () => {
+  useUser()
+    .getChats()
+    .then((res) => {
+      console.log(res, "chats");
+      for (let v of res) {
+        socket.emit("join", v._id);
+      }
+    });
+});
 
-const socket = new WebSocket('ws://localhost:4000');
+onMounted(() => {});
+
 const openChat = ref(null);
-const newMessage = ref('')
-const sockets = ref([])
-const chats = ref(useUserStore().chats)
-const openUserList = ref(false)
-const scrollChat = ref()
+const currentChatId = computed(() => {
+  if (!openChat.value._id) {
+    return null;
+  } else {
+    return openChat.value._id;
+  }
+});
+const newMessage = ref("");
+const sockets = ref([]);
+const chats = ref(useUserStore().chats);
+const openUserList = ref(false);
+const scrollChat = ref();
 
-function loguear(e){
-  console.log(' QUE MIERDAS PASA', e)
-  console.log('que cojones pasa tio', useUserStore().chats, useUserStore().chats.filter(c => c._id == e)[0], 'veamos a puto ver')
+function loguear(e) {
+  console.log(" QUE MIERDAS PASA", e);
+  console.log(
+    "que cojones pasa tio",
+    useUserStore().chats,
+    useUserStore().chats.filter((c) => c._id == e)[0],
+    "veamos a puto ver"
+  );
 }
 
 function r(e) {
-  console.log('que cojones pasa tio', useUserStore().chats, useUserStore().chats.filter(c => c._id == e)[0], 'veamos a puto ver')
-  loguear(e)
-  // useUser().getChats().then((res) => {
-  //   // openChat.value = useUserStore().chats.filter(c => c._id == e)[0]
-  //   console.log(openChat.value, useUserStore().chats, res, ' veamos a puto ver')
-  // })
-
+  console.log(
+    "que cojones pasa tio",
+    useUserStore().chats,
+    useUserStore().chats.filter((c) => c._id == e)[0],
+    "veamos a puto ver"
+  );
+  loguear(e);
 }
 
 function transformUIDToNick(uid, chat) {
-  return chat.participantes.filter(p => p._id == uid)[0].nick
+  return chat.participantes.filter((p) => p._id == uid)[0].nick;
 }
 
 async function testeo() {
-  console.log(chats.value, openChat.value._id, chats[openChat.value.id], chats.value)
   // openChat.value.mensajes.push({ autor: useUser().getUser()._id, contenido: newMessage.value })
-  await useUser().sendMessage({ autor: useUser().getUser()._id, content: newMessage.value, id: openChat.value._id })
+  await useUser().sendMessage({
+    autor: useUser().getUser()._id,
+    content: newMessage.value,
+    id: openChat.value._id,
+  });
+  socket.emit("new msg", {
+    autor: useUser().getUser()._id,
+    content: newMessage.value,
+    id: openChat.value._id,
+  });
   // chats.value[openChat.value.id].msg.push({ autor: 'admin', content: newMessage.value })
   // socket.send(JSON.stringify({ autor: 'admin', content: newMessage.value }))
   // sockets.value[openChat.value.id].send(JSON.stringify({ autor: 'admin', content: newMessage.value, id: openChat.value.id }))
-  newMessage.value = ''
-
-  console.log('vien', sockets.value, openChat.value, chats.value)
+  newMessage.value = "";
 }
 
-socket.onopen = () => {
-  console.log('Conectado');
-};
+// socket.on("connect", () => {
+//   console.log("Conectado");
+// });
 
 watch(openChat, (old) => {
-  console.log(openChat.value, 'cambiante')
+  console.log(openChat.value, "cambiante");
   if (openChat.value == null) {
     return;
   } else {
-
-    if (!sockets.value[openChat.value._id]) {
-      let newSocket = new WebSocket('wss://bondpetapi.duckdns.org');
-      newSocket.onerror = (e) => {
-        console.log(e, 'ERROR CATASTROFICO');
-      }
-      newSocket.onopen = () => {
-        newSocket.send(JSON.stringify({ id: openChat.value._id }))
-      }
-
-      newSocket.onmessage = async (e) => {
-        let data = JSON.parse(e.data);
-        useUserStore().chats.filter(c => c._id == data.id)[0].mensajes.push({ autor: data.autor, contenido: data.content })
-
-
-      };
-
-
-      sockets.value[openChat.value._id] = newSocket
-    }
   }
+  //   if (!sockets.value[openChat.value._id]) {
+  //     let newSocket = io(apiUrl);
+  //     newSocket.on("error", (e) => {
+  //       console.log(e, "ERROR CATASTROFICO");
+  //     });
+
+  //     newSocket.on("connect", () => {
+  //       console.log("Conectado en chat:", currentChatId.value);
+  //       newSocket.send(JSON.stringify({ id: openChat.value._id }));
+  //     });
+
+  //     newSocket.onmessage = async (e) => {
+  //       let data = JSON.parse(e.data);
+  //       useUserStore()
+  //         .chats.filter((c) => c._id == data.id)[0]
+  //         .mensajes.push({ autor: data.autor, contenido: data.content });
+  //     };
+
+  //     sockets.value[openChat.value._id] = newSocket;
+  //   }
+  // }
   nextTick(() => {
-    scrollChat.value.scrollTo(useUserStore().chats.filter(c => c._id == openChat.value._id)[0].mensajes.length)
-  })
-})
-
-
-
-socket.onmessage = (e) => {
-  console.log(e.data);
-}
-
-
-
-
-
-
+    scrollChat.value.scrollTo(
+      useUserStore().chats.filter((c) => c._id == openChat.value._id)[0]
+        .mensajes.length
+    );
+  });
+});
 </script>
 
 <style scoped lang="scss">
@@ -221,7 +292,6 @@ socket.onmessage = (e) => {
     }
 
     .chat-content {
-
       max-height: 42vh;
       // min-height: 42vh;
 
@@ -232,9 +302,7 @@ socket.onmessage = (e) => {
         max-height: 37.5vh;
         height: 100%;
       }
-
     }
-
   }
 
   .chats {
@@ -266,7 +334,6 @@ socket.onmessage = (e) => {
       bottom: 0;
       margin: 0.5rem;
     }
-
   }
 }
 </style>
--- pages/user/profile.vue
+++ pages/user/profile.vue
@@ -14,39 +14,13 @@
 
     <q-btn to="/pets/add-pet">Anadir mascota</q-btn>
 
-    <q-btn @click="send"> JODER YA</q-btn>
-    <q-btn @click="clo">vamos</q-btn>
-    <q-btn @click="conne">conectar </q-btn>
 </template>
 
 
 
 <script setup>
-import io from "socket.io-client"
-
-const socket = io(useRuntimeConfig().public.wsUrl)
-function send() {
-    socket.send({ id: '2', contenido: 'JODER YA' })
-}
-
-function conne(){
-    socket.connect()
-}
-
-function clo(){
-    socket.close()
-}
-
-socket.on('connect', () => {
-
-    socket.emit('join', 'user')
-    console.log('vA!!!!!!')
-})
-
-const apiUrl = useRuntimeConfig().public.urlApi
 const user = useUser().getUser();
-
-
+const apiUrl = useRuntimeConfig().public.urlApi
 
 definePageMeta({
     role: "user"
